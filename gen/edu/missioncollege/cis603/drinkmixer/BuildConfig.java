84b35cff56632d64c96d6afb788f47dbfc9abf3c
echo210 <echotehdolphin@gmail.com>
2013-10-09 13:26:52 -0700

    initial Commit
    The first commit.  I created the project called Drink Mixer and the
    first activity called Main.

---
+++ gen/edu/missioncollege/cis603/drinkmixer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.missioncollege.cis603.drinkmixer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
